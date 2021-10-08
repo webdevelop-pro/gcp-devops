b9eb9a3ba3afeccfc440834e5028c81f89770dc1
Georgy Shapchits <georgy.schapchits@synesis.ru>
2021-10-08 13:39:38 +0300

    feat/1442 Update backend-buckets script & add disable cdn method
    https://github.com/UnFederalReserve/development/issues/1442

--- scripts/buckets/backend-buckets.bash
+++ scripts/buckets/backend-buckets.bash
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 
-function create_bucket_lb() {
+function create_backend_bucket_lb() {
   SERVICE_NAME=$1
   DOMAIN_RECORD=$2
-
   BACKEND_BUCKET_NAME="${SERVICE_NAME}-${ENV_NAME}-backend-bucket"
+
   BUCKET_NAME="${PROJECT_NAME}-${ENV_NAME}-${SERVICE_NAME}"
   PATH_MATCHER_NAME="${ENV_NAME}-${SERVICE_NAME}-matcher"
 
-  gsutil mb -p ${PROJECT_ID} -c standard -b on gs://${BUCKET_NAME}
+  gsutil mb -p ${PROJECT_ID} -l ${BUCKETS_LOCATION} -c standard -b on gs://${BUCKET_NAME}
 
   gcloud compute backend-buckets create ${BACKEND_BUCKET_NAME} \
-      --gcs-bucket-name=${BUCKET_NAME}
+      --gcs-bucket-name=${BUCKET_NAME} \
+      --enable-cdn
 
   gcloud compute url-maps add-path-matcher ${HTTPS_LB_NAME} \
     --path-matcher-name=${PATH_MATCHER_NAME} \
@@ -19,24 +20,11 @@ function create_bucket_lb() {
     --default-backend-bucket=${BACKEND_BUCKET_NAME}
 }
 
-function main()
+function disable_cdn()
 {
-    FILE_NAME=$(basename $0)
-    SERVICE_NAME="${FILE_NAME%.*}"
+    SERVICE_NAME=$1
+    BACKEND_BUCKET_NAME="${SERVICE_NAME}-${ENV_NAME}-backend-bucket"
 
-    if [[ $ENV_NAME == "master" ]]
-    then
-        if [[ $SERVICE_NAME == "home"  ]]
-        then
-            export DOMAIN_RECORD="${DOMAIN}"
-        else
-            export DOMAIN_RECORD="${SERVICE_NAME}.${DOMAIN}"
-        fi
-    else
-        export DOMAIN_RECORD="${SERVICE_NAME}-${ENV_NAME}.${DOMAIN}"
-    fi
-
-    create_bucket_lb $SERVICE_NAME $DOMAIN_RECORD
+    gcloud compute backend-buckets update ${BACKEND_BUCKET_NAME} \
+        --no-enable-cdn
 }
-
-main $@
