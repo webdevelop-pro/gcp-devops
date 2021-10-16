de22a18dc5f84147f61fd9144698b59adca29ed3
Georgy Shapchits <georgy.schapchits@synesis.ru>
2021-10-16 15:59:48 +0300

    Update scripts

--- scripts/dns/records.bash
+++ scripts/dns/records.bash
@@ -1,30 +1,49 @@
 #!/usr/bin/env bash
 
+DNS_ZONE_NAME=$(echo "${env_project_domain}" | tr -d '.-_')
+STATIC_IP_NAME="public-frontend-ip"
+
 function create_dns_record()
 {
   _DOMAIN_RECORD=$1
 
-  _CURRENT_DOMAIN=$(gcloud dns record-sets list --zone ${DNS_ZONE_NAME} --format json | jq ".[] | select(.name==\"${_DOMAIN_RECORD}.\")")
-  _IP=$(gcloud compute addresses describe --global --format json ${STATIC_IP_NAME} | jq '.address' | tr -d '"')
+  _CURRENT_DOMAIN=$(gcloud dns record-sets list --project ${env_project_id} --zone ${DNS_ZONE_NAME} --format json | jq ".[] | select(.name==\"${_DOMAIN_RECORD}.\")")
+  _IP=$(gcloud compute addresses describe --global --project ${env_project_id} --format json ${STATIC_IP_NAME} | jq '.address' | tr -d '"')
 
   if [[ ${_CURRENT_DOMAIN} == "" ]]
   then
     rm /tmp/transaction.yaml
 
-    gcloud dns record-sets transaction start --zone=${DNS_ZONE_NAME} --transaction-file=/tmp/transaction.yaml
+    gcloud dns record-sets transaction start \
+      --project ${env_project_id} \
+      --zone=${DNS_ZONE_NAME} \
+      --transaction-file=/tmp/transaction.yaml
 
     gcloud dns record-sets transaction add ${_IP} \
+      --project ${env_project_id} \
       --name=${_DOMAIN_RECORD} \
       --type=A \
       --ttl 300 \
       --zone=${DNS_ZONE_NAME} \
       --transaction-file=/tmp/transaction.yaml
 
-    gcloud dns record-sets transaction execute --zone=${DNS_ZONE_NAME} --transaction-file=/tmp/transaction.yaml
+    gcloud dns record-sets transaction execute \
+      --project ${env_project_id} \
+      --zone=${DNS_ZONE_NAME} \
+      --transaction-file=/tmp/transaction.yaml
   else
     gcloud dns record-sets update ${_DOMAIN_RECORD} \
+      --project ${env_project_id} \
       --rrdatas ${_IP} \
       --type=A \
       --zone ${DNS_ZONE_NAME}
   fi
+}
+
+function create_dns_zone()
+{
+  gcloud dns managed-zones create ${DNS_ZONE_NAME} \
+    --project ${env_project_id} \
+    --dns-name "${env_project_domain}." \
+    --description "${env_project_domain} dns zone"
 }
--- scripts/lb/https.bash
+++ scripts/lb/https.bash
@@ -1,23 +1,35 @@
 #!/usr/bin/env bash
 
+REDIRECT_NAME="apps-https-redirect"
+HTTPS_LB_NAME="apps-https-lb"
+STATIC_IP_NAME="public-frontend-ip"
+PROXY_NAME="https-lb-proxy"
+CERTIFICATE_NAME="frontend-certificate"
+LB_RULE_NAME="https-lb-rule"
+PROJECT_NAME="${env_project_id}"
+
 function create_https_lb()
 {
   export DEFAULT_BACKEND_BUCKET_NAME="${PROJECT_NAME}-default-backend"
-  export DEFAULT_BUCKET_NAME=${PROJECT_NAME}-${ENV_NAME}-default
+  export DEFAULT_BUCKET_NAME=${PROJECT_NAME}-${env_name}-default
 
-  gsutil mb -p ${PROJECT_ID} -c standard -b on gs://${DEFAULT_BUCKET_NAME}
+  gsutil mb -p ${env_project_id} -c standard -b on gs://${DEFAULT_BUCKET_NAME}
 
   gcloud compute backend-buckets create ${DEFAULT_BACKEND_BUCKET_NAME} \
+      --project ${env_project_id} \
       --gcs-bucket-name=${DEFAULT_BUCKET_NAME}
 
   gcloud compute url-maps create ${HTTPS_LB_NAME} \
+      --project ${env_project_id} \
       --default-backend-bucket=${DEFAULT_BACKEND_BUCKET_NAME}
 
   gcloud compute target-https-proxies create ${PROXY_NAME} \
+    --project ${env_project_id} \
     --url-map=${HTTPS_LB_NAME} \
     --ssl-certificates=${CERTIFICATE_NAME}
 
   gcloud compute forwarding-rules create ${LB_RULE_NAME} \
+      --project ${env_project_id} \
       --address=${STATIC_IP_NAME} \
       --global \
       --target-https-proxy=${PROXY_NAME} \
