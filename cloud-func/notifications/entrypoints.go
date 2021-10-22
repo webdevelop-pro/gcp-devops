cde77a865946ebe9d05662d0ec8e7d8085dce879
Vlad Tarasenko <vladka@webdevelop.pro>
2021-10-22 19:57:14 +0300

    moved pubsub stuff to messages

--- cloud-func/notifications/entrypoints.go
+++ cloud-func/notifications/entrypoints.go
@@ -4,9 +4,10 @@ import (
 	"context"
 
 	"github.com/webdevelop-pro/gcp-devops/cloud-func/notifications/cloudbuild"
+	"github.com/webdevelop-pro/gcp-devops/cloud-func/notifications/messages"
 )
 
 // Subscribe consumes a Pub/Sub message.
-func CloudBuildSubscribe(ctx context.Context, m cloudbuild.PubSubMessage) error {
+func CloudBuildSubscribe(ctx context.Context, m messages.PubSubMessage) error {
 	return cloudbuild.Subscribe(ctx, m)
 }
