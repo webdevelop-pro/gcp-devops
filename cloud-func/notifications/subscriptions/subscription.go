4d865a8e2578851cdffb7e70fe8284ecf8f0867e
Vlad Tarasenko <vladka@webdevelop.pro>
2021-10-22 18:32:47 +0300

    updated locations for cloudbuild

--- cloud-func/notifications/subscriptions/subscription.go
+++
@@ -1,7 +0,0 @@
-package subscriptions
-
-// PubSubMessage is the payload of a Pub/Sub event. Please refer to the docs for
-// additional information regarding Pub/Sub events.
-type PubSubMessage struct {
-	Data []byte `json:"data"`
-}
