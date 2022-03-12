4095f0641d26d386bc1640014e02462bcc6346c2
Georgy Shapchits <georgy.schapchits@synesis.ru>
2022-03-12 19:02:45 +0400

    Update records script

--- scripts/dns/records.bash
+++ scripts/dns/records.bash
@@ -4,20 +4,18 @@ DNS_ZONE_NAME=$(echo "${env_project_domain}" | tr -d '.-_')
 
 function create_dns_record()
 {
-  _DOMAIN_RECORD=$1
-  _IP=$2
-  _TYPE=$3
-  _TTL=$
+  _TYPE=$1
+  _DOMAIN_RECORD=$2
+  _IP=$3
+  _TTL=$4
 
-  if [[ ${_TYPE} == "" ]]; than
-    _TYPE="A"
-  fi
+  echo "Create DNS record: ${_TYPE} ${_DOMAIN_RECORD} ${_IP} ${_TTL}"
 
-  if [[ ${_TTL} == "" ]]; than
+  if [[ ${_TTL} == "" ]]; then
     _TTL=300
   fi
 
-  _CURRENT_DOMAIN=$(gcloud dns record-sets list --project ${env_project_id} --zone ${DNS_ZONE_NAME} --format json | jq ".[] | select(.name==\"${_DOMAIN_RECORD}.\")")
+  _CURRENT_DOMAIN=$(gcloud dns record-sets list --project ${env_project_id} --zone ${DNS_ZONE_NAME} --format json | jq ".[] | select(.name==\"${_DOMAIN_RECORD}.\") | select(.type==\"${_TYPE}\")")
 
   if [[ ${_CURRENT_DOMAIN} == "" ]]
   then
@@ -28,22 +26,30 @@ function create_dns_record()
       --zone=${DNS_ZONE_NAME} \
       --transaction-file=/tmp/transaction.yaml
 
-    gcloud dns record-sets transaction add ${_IP} \
+    gcloud dns record-sets transaction add \
       --project ${env_project_id} \
       --name=${_DOMAIN_RECORD} \
       --type=${_TYPE} \
       --ttl=${_TTL} \
       --zone=${DNS_ZONE_NAME} \
-      --transaction-file=/tmp/transaction.yaml
+      --transaction-file=/tmp/transaction.yaml \
+      -- "${_IP}"
 
     gcloud dns record-sets transaction execute \
       --project ${env_project_id} \
       --zone=${DNS_ZONE_NAME} \
       --transaction-file=/tmp/transaction.yaml
+
+    # For multiline CNAME, please don't remove this =)
+    gcloud dns record-sets update ${_DOMAIN_RECORD} \
+      --project ${env_project_id} \
+      --rrdatas "${_IP}" \
+      --type=${_TYPE} \
+      --zone ${DNS_ZONE_NAME}
   else
     gcloud dns record-sets update ${_DOMAIN_RECORD} \
       --project ${env_project_id} \
-      --rrdatas ${_IP} \
+      --rrdatas "${_IP}" \
       --type=${_TYPE} \
       --zone ${DNS_ZONE_NAME}
   fi
