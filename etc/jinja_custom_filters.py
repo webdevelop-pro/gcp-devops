880d6570f755d3d33d0a7671c3c44a1f5da5b684
Goginet <gogi.soft.gm@gmail.com>
2022-12-25 19:25:06 +0300

    Add generate_htpasswd function

--- etc/jinja_custom_filters.py
+++ etc/jinja_custom_filters.py
@@ -1,5 +1,6 @@
 import base64
 import json
+import bcrypt
 
 def b64encode(s):
     return base64.b64encode(s.encode()).decode()
@@ -11,3 +12,7 @@ def to_json_escape(s):
     ss = json.dumps(s)
     sss = json.dumps(ss)
     return sss[1:-1]
+
+def generate_htpasswd(username, password):
+    bcrypted = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt(rounds=12)).decode("utf-8")
+    return f"{username}:{bcrypted}"
