13e69919a8a4a1aa2713f058e1663cb3982333c4
Goginet <gogi.soft.gm@gmail.com>
2023-02-05 16:54:12 +0300

    Refactore render_template

--- make.sh
+++ make.sh
@@ -24,20 +24,63 @@ if [ -d ${BASE_PATH}/../configs/secrets/${env_secrets} ]; then
     ENV_SECRETS=${BASE_PATH}/../configs/secrets/${env_secrets}
 fi
 
-function render_template()
+
+function _render_template()
 {
     SERVICE_NAME=$1
     TEMPLATE_FILE=$2
-    j2 --filters ${BASE_PATH}/etc/jinja_custom_filters.py -e os "${TEMPLATES_DIR}/${TEMPLATE_FILE}" ${DEPLOY_CONFIG} | sed '/^[[:blank:]]*$/ d' > ${RENDERED_TEMPLATES_DIR}/${SERVICE_NAME}/${TEMPLATE_FILE}
+
+    echo "Render $(basename $TEMPLATE_FILE) for $SERVICE_NAME"
+
+    j2 --filters ${BASE_PATH}/etc/jinja_custom_filters.py -e os "${TEMPLATE_FILE}" ${DEPLOY_CONFIG} | sed '/^[[:blank:]]*$/ d' > ${RENDERED_TEMPLATES_DIR}/${SERVICE_NAME}/$(basename ${TEMPLATE_FILE})
+
+    if [ ${PIPESTATUS[0]} -ne 0 ]; then
+        echo "!!!!!!!!!! Failed while render $(basename $TEMPLATE_FILE) template for service ${SERVICE_NAME}"
+        exit 1
+    fi
 }
 
-function render_templates()
+function _get_service_config()
 {
+    SERVICE_NAME=$1
+
+    if [[ ${SERVICE_NAME} = "" ]]; then
+        SERVICE_NAME="all"
+    fi
+
+    if [[ $( echo ${SERVICE_NAME} | tr '[:upper:]' '[:lower:]' ) = 'all' ]]; then
+        SERVICE_NAME='*'
+    fi
+
+    find ${GLOBAL_CONFIGS} ${ENV_SECRETS} -type f -name "${SERVICE_NAME}.yaml"
+}
+
+function _get_template_file()
+{
+    GROUP=$1
+    TEMPLATE_NAME=$2
+
+    if [[ ${TEMPLATE_NAME} = "" ]]; then
+        TEMPLATE_NAME="all"
+    fi
+
+    if [[ $( echo ${TEMPLATE_NAME} | tr '[:upper:]' '[:lower:]' ) = 'all' ]]; then
+        TEMPLATE_NAME='*'
+    fi
+
+    find ${TEMPLATES_DIR}/${GROUP} -type f -name "${TEMPLATE_NAME}.yaml"
+}
+
+function _render_app_templates()
+{
+    SERVICE_NAME=$1
+    TEMPLATE_NAME=$2
+
     sed 's/{{/((/g' ${DEPLOY_CONFIG} | sed 's/}}/))/g' > /tmp/new_deploy.yaml
 
     cp /tmp/new_deploy.yaml ${DEPLOY_CONFIG}
 
-    for FILENAME in $(find ${GLOBAL_CONFIGS} ${ENV_SECRETS} -type f)
+    for FILENAME in $(_get_service_config ${SERVICE_NAME})
     do
         export filename=$(basename ${FILENAME})
 
@@ -62,7 +105,7 @@ function render_templates()
 
     rm -rf ${RENDERED_TEMPLATES_DIR}
 
-    for FILENAME in $(find ${GLOBAL_CONFIGS} ${ENV_SECRETS} -type f)
+    for FILENAME in $(_get_service_config ${SERVICE_NAME})
     do
         FILE_NAME=$(basename ${FILENAME})
         export filename=${FILE_NAME}
@@ -70,34 +113,41 @@ function render_templates()
         OUTPUT_DIR=apps/${FILE_NAME}
         mkdir -p ${RENDERED_TEMPLATES_DIR}/${OUTPUT_DIR}
 
-        # Render configmap for app
-        render_template ${OUTPUT_DIR} configmap.yaml
-
-        # Render secret for app
-        render_template ${OUTPUT_DIR} secret.yaml
-
-        # Render deployment for app
-        render_template ${OUTPUT_DIR} deployment.yaml
-
-        # Render cronjob for app
-        render_template ${OUTPUT_DIR} cronjob.yaml
-
-        # Render service for app
-        render_template ${OUTPUT_DIR} service.yaml
-
-        # Render ingress for app
-        render_template ${OUTPUT_DIR} ingress.yaml
-
-        # Render certificate for app
-        render_template ${OUTPUT_DIR} certificate.yaml
-
-        # Render namespace for app
-        render_template ${OUTPUT_DIR} namespace.yaml
+        for TEMPLATE_FILE in $(_get_template_file apps ${TEMPLATE_NAME})
+        do
+            _render_template ${OUTPUT_DIR} ${TEMPLATE_FILE}
+        done
     done
+
+    # mkdir -p ${RENDERED_GLOBAL_DIR}
+
+    # render_template global letsencrypt-issuer.yaml
+
+    # Delete empty files
+    find ${RENDERED_TEMPLATES_DIR} -size 0 -delete
+}
+
+function _render_global_templates() {
+    TEMPLATE_NAME=$1
 
     mkdir -p ${RENDERED_GLOBAL_DIR}
 
-    render_template global letsencrypt-issuer.yaml
+    for TEMPLATE_FILE in $(_get_template_file global ${TEMPLATE_NAME})
+    do
+        _render_template global ${TEMPLATE_FILE}
+    done
+}
+
+function render_templates()
+{
+    SERVICE_NAME=$1
+    TEMPLATE_NAME=$2
+
+    if [[ ${SERVICE_NAME} = "global" ]]; then
+        _render_global_templates $TEMPLATE_NAME
+    else
+        _render_app_templates $SERVICE_NAME $TEMPLATE_NAME
+    fi
 
     # Delete empty files
     find ${RENDERED_TEMPLATES_DIR} -size  0 -print -delete
@@ -267,6 +317,17 @@ EOF
      --env-vars-file=/tmp/cloudbuild.env
 }
 
-get_credentials
+function help()
+{
+    echo "Usage:
+        ./deploy_apps.bash <command> [arguments]
+
+    Commands:
+        render_templates        Render templates for k8s manifests per service
+        apply                   Apply rendered templates to k8s
+        deploy                  Render + Apply
+        help                    Show help
+    "
+}
 
 $@
