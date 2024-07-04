7a7f6fa02cf548db496d1bebc33269b5509a8a0c
Vlad <vladka@webdevelop.pro>
2024-07-04 21:57:15 +0300

    moved cloud images to separate repo

--- cloud-builders/yarn/deploy.sh
+++
@@ -1,5 +0,0 @@
-docker build -t gcr.io/unfederalreserve-dev/yarn:`date +%d%m%Y` .
-docker push gcr.io/unfederalreserve-dev/yarn:`date +%d%m%Y`
-
-docker build -t gcr.io/unfederalreserve-dev/yarn:latest .
-docker push gcr.io/unfederalreserve-dev/yarn:latest
